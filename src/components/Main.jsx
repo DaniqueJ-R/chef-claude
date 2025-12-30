19df2253d54b2fb1ed5ccdf4f0f9f45ae6cbde4c
DaniqueJ-R <24005981@student.peterborough.ac.uk>
2025-12-30 15:39:39 +0000

    added Main section

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return(
+        <main>
+            <section className="add-ingredient-form">
+                <input type="" placeholder="e.g. salmon" />
+                <button type="submit"> Add ingredient</button>
+            </section>
+            
+        </main>
+    )
+}
