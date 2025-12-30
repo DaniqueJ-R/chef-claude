ffbdc15ac2360ba020ce30cf3c035ff3b4413f8a
DaniqueJ-R <24005981@student.peterborough.ac.uk>
2025-12-30 15:31:17 +0000

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return(
+        <header>
+            <img src="src\assets\logo.png" alt="chef claude logo" />
+            <h1> Chef Claude</h1>
+        </header>
+    )
+}
