c9461d674ecc5986b6ae64da5b1f45e7bf68c92d
DaniqueJ-R <24005981@student.peterborough.ac.uk>
2026-01-01 19:05:42 +0000

    added update list function

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,33 @@
-export default function Header() {
+import React from 'react' 
+
+export default function Main() {
+
+    const [ingredients, setIngredients] = React.useState(["Chicken", "Lime", "Wraps"])
+
+    const ingredientList = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleForm(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+
     return(
         <main>
-            <section className="add-ingredient-form">
-                <input type="" placeholder="e.g. salmon" />
+            <form onSubmit={handleForm} className="add-ingredient-form">
+                <input type="text" 
+                name="ingredient"
+                placeholder="e.g. salmon" 
+                aria-label="Add Ingredient" />
                 <button type="submit"> Add ingredient</button>
-            </section>
+            </form>
+
+            <ul>
+                {ingredientList}
+            </ul>
             
         </main>
     )
