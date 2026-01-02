ec6a3c3e8b2d61f8e22bfbcacad852c76d449dec
DaniqueJ-R <24005981@student.peterborough.ac.uk>
2026-01-02 20:38:18 +0000

    added logic to show list with get recipe button

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Main() {
 
-    const [ingredients, setIngredients] = React.useState(["Chicken", "Lime", "Wraps"])
+    const [ingredients, setIngredients] = React.useState([])
 
     const ingredientList = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
@@ -23,9 +23,19 @@ export default function Main() {
                 <button type="submit"> Add ingredient</button>
             </form>
 
-            <ul>
-                {ingredientList}
-            </ul>
+           {ingredients.length === 0? <ul className="ingredients-list" aria-live="polite">{ingredientList}</ul>: 
+                
+           <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientList}</ul>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section>}
             
         </main>
     )
