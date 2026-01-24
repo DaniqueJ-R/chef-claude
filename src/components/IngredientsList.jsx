6c1d5990fd34aa4bbfc0203fc48598261dfe525d
DaniqueJ-R <24005981@student.peterborough.ac.uk>
2026-01-24 21:33:00 +0000

    updated ingredients component

--- src/components/IngredientsList.jsx
+++ src/components/IngredientsList.jsx
@@ -1,8 +1,28 @@
+    
+
 export default function IngredientsList (props) {
 
-    return(
-        <li key={props.ingredient}>{props.ingredient}</li>
 
+        const ingredientList = props.ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    )) 
+
+    return(
+        props.ingredients.length < 4 ? <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientList}</ul></section>: 
+                
+           <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientList}</ul>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button type='submit' onClick={props.clickFunc}>Get a recipe</button>
+                </div>
+            </section>
     )
     
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -22,7 +22,7 @@ export default function Main() {
      * done, so there will likely be some extra work to be done
      * beyond what I've listed above.
      */
-    const [ingredients, setIngredients] = React.useState(['fish']) //tay
+    const [ingredients, setIngredients] = React.useState(['fish','lime','rice']) 
 
 
     const [recipeShown, setRecipeShown] = React.useState(false)
@@ -30,11 +30,6 @@ export default function Main() {
     function recipeToggle () {
         setRecipeShown(prevShown => !prevShown)
     }
-
-
-    const ingredientList = ingredients.map(ingredient => (
-        <IngredientsList key={ingredient} ingredient={ingredient} />
-    )) 
 
     function addIngredients(formData) {
         const newIngredient = formData.get("ingredient")
@@ -51,19 +46,8 @@ export default function Main() {
                 <button type="submit"> Add ingredient</button>
             </form>
 
-            {ingredients.length < 4 ? <ul className="ingredients-list" aria-live="polite">{ingredientList}</ul>: 
-                
-           <section>
-                <h2>Ingredients on hand:</h2>
-                <ul className="ingredients-list" aria-live="polite">{ingredientList}</ul>
-                <div className="get-recipe-container">
-                    <div>
-                        <h3>Ready for a recipe?</h3>
-                        <p>Generate a recipe from your list of ingredients.</p>
-                    </div>
-                    <button type='submit' onClick={recipeToggle}>Get a recipe</button>
-                </div>
-            </section>}
+            <IngredientsList ingredients={ingredients} clickFunc={recipeToggle}/>
+            
 
             {recipeShown && <ClaudeRecipe />}
         </main>
