420a4ad6dd0703f0f85f3a54055080a58381124f
DaniqueJ-R <24005981@student.peterborough.ac.uk>
2026-01-25 01:08:24 +0000

    removed comments

--- src/components/IngredientsList.jsx
+++ src/components/IngredientsList.jsx
@@ -26,16 +26,3 @@ export default function IngredientsList (props) {
     )
     
 }
-        // {ingredients.length < 4 ? <ul className="ingredients-list" aria-live="polite">{ingredientList}</ul>: 
-                
-        //    <section>
-        //         <h2>Ingredients on hand:</h2>
-        //         <ul className="ingredients-list" aria-live="polite">{ingredientList}</ul>
-        //         <div className="get-recipe-container">
-        //             <div>
-        //                 <h3>Ready for a recipe?</h3>
-        //                 <p>Generate a recipe from your list of ingredients.</p>
-        //             </div>
-        //             <button type='submit' onClick={recipeToggle}>Get a recipe</button>
-        //         </div>
-        //     </section>}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,25 +3,7 @@ import ClaudeRecipe from './ClaudeRecipe'
 import IngredientsList from './IngredientsList'
 
 export default function Main() {
-    /**
-     * Challenge: clean up our code!
-     * Let's make a couple new components to make things a
-     * little cleaner. (Notice: I'm not suggesting what we
-     * have now is bad or wrong. I'm mostly finding an excuse
-     * to get in some hands-on practice 🙂)
-     * 
-     * 2. Move the list of ingredients <section> into its
-     *    own IngredientsList component.
-     * 
-     * While you're considering how to structure things, consider
-     * where state is, think about if it makes sense or not to
-     * move it somewhere else, how you'll communicate between
-     * the parent/child components, etc.
-     * 
-     * The app should function as it currently does when you're
-     * done, so there will likely be some extra work to be done
-     * beyond what I've listed above.
-     */
+
     const [ingredients, setIngredients] = React.useState([]) 
 
 
